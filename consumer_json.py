4933cb5d2096291296992638f481b4632da95237
Simone <difrancosimone@gmail.com>
2021-05-23 17:19:03 +0200

    add json consumer

---
+++ consumer_json.py
@@ -0,0 +1,26 @@
+from kafka import KafkaConsumer
+import msgpack
+import json
+
+# To consume latest messages and auto-commit offsets
+consumer = KafkaConsumer('json-topic',
+                         group_id='my-group',
+                         bootstrap_servers=['localhost:9092'])
+for message in consumer:
+    # message value and key are raw bytes -- decode if necessary!
+    # e.g., for unicode: `message.value.decode('utf-8')`
+    print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
+                                         message.offset, message.key,
+                                         message.value))
+
+# consume earliest available messages, don't commit offsets
+KafkaConsumer(auto_offset_reset='earliest', enable_auto_commit=False)
+
+# consume json messages
+KafkaConsumer(value_deserializer=lambda m: json.loads(m.decode('ascii')))
+
+# consume msgpack
+KafkaConsumer(value_deserializer=msgpack.unpackb)
+
+# StopIteration if no message after 1sec
+KafkaConsumer(consumer_timeout_ms=1000)
